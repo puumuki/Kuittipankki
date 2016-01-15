b0e3cb139391d887f9db1769fd1e795d30808ba3
Teemu Puukko <teemuki@gmail.com>
2016-01-16 00:27:37 +0200

    Portti jossa sovellus käynnistyy luetaan nyt settings-tiedostosta.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-var logging = require('./logging')
+var logging = require('./logging');
 var app = require('./app');
+var settings = require('./settings');
 
-var server = app.listen(8080, function () {
+var server = app.listen(settings.port, function () {
   var host = server.address().address;
   var port = server.address().port;
   logging.info('Kuittipankki starting at http://%s:%s', host, port);
