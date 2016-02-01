77c3bac3596e73f48d721d08cca9fe926bca10d2
Teemu Puukko <teemuki@gmail.com>
2016-02-01 18:41:51 +0200

    More documentation and starting to use logging.

--- server/picture-service.js
+++ server/picture-service.js
@@ -1,12 +1,19 @@
-
+/**
+ * Picture service module contains file operations required to store, delete and list
+ * pictures at the disk.
+ */
 var _ = require('underscore');
 var fs = require('fs');
 var path = require('path');
+var logging = require('./logging');
 
 /**
  * Load pictures all pictures
+ * @return array of objects containing all pictures in pictures directory
  */
 function loadPictures() {
+
+  //Supported file formats
   const fileTypes = ['bmp','png','jpg'];
 
   var files = fs.readdirSync(path.join(__dirname,'pictures'));
@@ -26,28 +33,46 @@ function loadPictures() {
   });
 }
 
-function filterPicturesByReceiptID(id, files) {
-  return _.filter(files, function(file) {
+/**
+ * Picture's filename has a receipt ID included, by that
+ * information receipts are filtered from file array
+ * @param {string} receiptID
+ * @param {array of objects} pictures 
+ * @return {array of objects} return pictures that has receiptID in filename
+ */
+function filterPicturesByReceiptID(id, pictures) {
+  return _.filter(pictures, function(file) {
     var parts = file.filename.split('.');
     return _.first(parts) === id;
   });
 }
 
-function filterPicturesByFilename(filename, files) {
-  var results = _.filter(files, function(file) {
+/**
+ * Filters pictures by the filename
+ * @param {string} filename
+ * @param {array of objects} pictures
+ * @return {object} picture object is one is found, it not returns undefined
+ */
+function filterPicturesByFilename(filename, pictures) {
+  var results = _.filter(pictures, function(file) {
     return file.filename === filename;
   });
 
   return _.first(results);
 }
 
+/**
+ * Deletes picture from the disk, deletion is a syncronous operation.
+ * @param {object} picture object
+ * @return undefined
+ */
 function deletePicture(picture) {
   var picturePath = path.join(__dirname,'pictures', picture.filename);
-  console.log("Deleting picture", picturePath);
+  logging.log("Deleting picture", picturePath);
   fs.unlinkSync(picturePath);
   
   var thumbnailPath = path.join(__dirname,'pictures', picture.thumbnail);
-  console.log("Deleting thumbnail", thumbnailPath);
+  logging.log("Deleting thumbnail", thumbnailPath);
   fs.unlinkSync(thumbnailPath);
 }
 
