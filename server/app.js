35ca5a9826601771831fb713cb3be998a3acb884
Teemu Puukko <teemuki@gmail.com>
2016-03-07 21:59:20 +0200

    Poistin handlebars template logiikan serverin puolelta.
    Poistin handlebars template logiikan serverin puolelta. Ihan vain
    yksinkertaistaakseni palvelinpuolta.

--- server/app.js
+++ server/app.js
@@ -96,8 +96,9 @@ app.use(function(req, res, next) {
 
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+      logging.error(err);
         res.status(err.status || 500);
-        res.render('error', {
+        res.render({
             message: err.message,
             error: err,
             title: 'error'
@@ -109,7 +110,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render({
         message: err.message,
         error: {},
         title: 'error'
