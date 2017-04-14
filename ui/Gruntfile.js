2ff425be8acda0e47c779be53e25195e5d1d3763
Teemu Puukko <teemuki@gmail.com>
2017-04-14 12:13:44 +0300

    Includes Handlebars as a part compiled software
    Includes Handlebars as a part compiled software, this allows runtime
    compilation of handlebars templates.

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -130,11 +130,11 @@ module.exports = function (grunt) {
                     pragmasOnSave: {
                         //removes Handlebars.Parser code (used to compile template strings) set
                         //it to `false` if you need to parse template strings even after build
-                        excludeHbsParser : true,
+                        excludeHbsParser : false,
                         // kills the entire plugin set once it's built.
-                        excludeHbs: true,
+                        excludeHbs: false,
                         // removes i18n precompiler, handlebars and json2
-                        excludeAfterBuild: true
+                        excludeAfterBuild: false
                     },
                     wrapShim: false,
                     out:'build/scripts/kuittipankki.js'
